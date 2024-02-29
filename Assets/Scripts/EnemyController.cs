a863a4105a5a3617a377742c9e33d59dcad857a6
IgnacioPerez21 <ipersan3006@g.educaand.es>
2024-02-29 19:37:19 +0100

    Update Proyectiles

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 3f;
+    public float speed = 25f;
 
     void Update()
     {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 40f;
+    public GameObject projectilePrefab; // Referencia al prefab del proyectil
 
     void Update()
     {
@@ -13,5 +14,23 @@ public class PlayerController : MonoBehaviour
 
         Vector2 movement = new Vector2(horizontal, vertical);
         transform.Translate(movement * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Fire1")) // Cambia "Fire1" según tu configuración de entrada
+        {
+            // Llama a la función para lanzar el proyectil
+            FireProjectile();
+        }
+    }
+
+    void FireProjectile()
+    {
+        // Instancia el prefab del proyectil en la posición y rotación actual del jugador
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+
+        // Obtén el componente Rigidbody2D del proyectil
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+
+        // Aplica fuerza al proyectil en la dirección hacia adelante del jugador
+        rb.AddForce(transform.up * speed, ForceMode2D.Impulse);
     }
 }
